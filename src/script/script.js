164bac38d77e99677be5be79d26991d3ce952c14
x5sh1 <2319012176@qq.com>
2023-07-26 00:38:43 +0800

    增加js

---
+++ src/script/script.js
@@ -0,0 +1,8 @@
+const svgConst = document.body.querySelector("svg");
+const cPlayer = document.body.createElement("circle");
+cPlayer.setAttribute("cx", "250");
+cPlayer.setAttribute("cy", "400");
+cPlayer.setAttribute("r", "20");
+cPlayer.setAttribute("stroke", "black");
+cPlayer.setAttribute("fill", "none");
+svgConst.appendChild(cPlayer);
