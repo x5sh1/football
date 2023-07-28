15f950a9b954ea562868b64e3f1ce261877cb322
韩山 <hanshan@xiaomi.com>
2023-07-28 19:02:08 +0800

    js增加SVG元素

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,10 @@
 const svgConst = document.body.querySelector("svg");
-const cPlayer = document.body.createElement("circle");
+const cPlayer = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 cPlayer.setAttribute("cx", "250");
 cPlayer.setAttribute("cy", "400");
 cPlayer.setAttribute("r", "20");
 cPlayer.setAttribute("stroke", "black");
 cPlayer.setAttribute("fill", "none");
-svgConst.appendChild(cPlayer);
+svgConst.append(cPlayer);
+
+console.log(svgConst.querySelector("circle").constructor);
